97da2f4979448738a99ae1fa40deae536d4cc707
Frank Endriss <fendriss@fum.de>
2014-03-05 07:53:36 +0100

    Generated spring files had not closing "</beans>" tag. fixed.

--- swt/codeGenerator/src/main/java/net/neobp/dev/codeGenerator/SpringFileTemplate.java
+++ swt/codeGenerator/src/main/java/net/neobp/dev/codeGenerator/SpringFileTemplate.java
@@ -18,12 +18,15 @@ public class SpringFileTemplate
     public String toString() {
         final StringBuilder sb=new StringBuilder();
         sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+
         sb.append("<beans xmlns=\"http://www.springframework.org/schema/beans\"\n");
         sb.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
         sb.append("xsi:schemaLocation=\"http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd\">\n\n");
 
         for(XmlTag xmlTag : tags)
             sb.append(xmlTag.toString());
+
+        sb.append("</beans>");
         return  sb.toString();
     }
 
